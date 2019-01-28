c676247a70be2ac2e62c02d8f57ba3e232821637
David Joy <djoy@C02XQ24LJG5J.tld>
2019-01-28 17:03:52 -0500

    Holy padding, Batman.

--- src/DocSection.js
+++ src/DocSection.js
@@ -5,8 +5,8 @@ export default function DocSection(props) {
   return (
     <React.Fragment>
       <a href={`#${props.id}`} id={props.id} />
-      <section className="pt-5 pb-5">
-        <h3 className="mb-5">{props.title}</h3>
+      <section className="pt-3 pb-3">
+        <h3 className="mb-3">{props.title}</h3>
         {props.children}
       </section>
     </React.Fragment>

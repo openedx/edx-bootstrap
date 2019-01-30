bd52ed8d114f3740aa4982fe7e7043288d56806d
Adam Butterworth <abutterworth@edx.org>
2019-01-30 18:48:06 -0500

    Updates

---
+++ precache-manifest.04f14888163936f60e5ffa4884b406ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aafdef785290813156a2",
+    "url": "/edx-bootstrap/static/css/main.19425449.chunk.css"
+  },
+  {
+    "revision": "aafdef785290813156a2",
+    "url": "/edx-bootstrap/static/js/main.aafdef78.chunk.js"
+  },
+  {
+    "revision": "b3b452d0eed421bc28a6",
+    "url": "/edx-bootstrap/static/js/1.b3b452d0.chunk.js"
+  },
+  {
+    "revision": "cdd8176117af8ea60bdd",
+    "url": "/edx-bootstrap/static/js/runtime~main.cdd81761.js"
+  },
+  {
+    "revision": "480dbf2988571e388cc6c68b3dccf49e",
+    "url": "/edx-bootstrap/index.html"
+  }
+];

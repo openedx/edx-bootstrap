739925124fe131bd3b2fb2ccdaef5b888e2279dc
David Joy <djoy@C02XQ24LJG5J.tld>
2019-01-30 16:06:42 -0500

    Breadcrumb and Badge pages complete (for now).

--- src/pages/bootstrap/Badge.js
+++ src/pages/bootstrap/Badge.js
@@ -4,25 +4,27 @@ export default class Badge extends React.Component {
   render() {
     return (
 <main className="col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content" role="main">
-  <h1 className="bd-title" id="content">Badges</h1>
-  <p className="bd-lead">Documentation and examples for badges, our small count and labeling component.</p>
+  <h1 className="bd-title" id="content">Badges / Tags</h1>
+  
+  <h2>Notes</h2>
+  <ul>
+    <li>Our default badges are pills.  There's no need to use the 'badge-pill' class.</li>
+  </ul>
   
   <h2>Todo:</h2>
   <ul>
-    <li>Make default .badge class be pills</li>
     <li>Abby: to provide design</li>
   </ul>
 
-  <h2 id="pill-badges"><span className="bd-content-title">Pill badges /tags</span></h2>
-  <div className="bd-example">
-    <span className="badge badge-pill badge-primary">Primary</span>
-    <span className="badge badge-pill badge-secondary">Secondary</span>
-    <span className="badge badge-pill badge-success">Success</span>
-    <span className="badge badge-pill badge-danger">Danger</span>
-    <span className="badge badge-pill badge-warning">Warning</span>
-    <span className="badge badge-pill badge-info">Info</span>
-    <span className="badge badge-pill badge-light">Light</span>
-    <span className="badge badge-pill badge-dark">Dark</span>
+  <div className="bd-example d-flex justify-content-between">
+    <span className="badge badge-primary">Primary</span>
+    <span className="badge badge-secondary">Secondary</span>
+    <span className="badge badge-success">Success</span>
+    <span className="badge badge-danger">Danger</span>
+    <span className="badge badge-warning">Warning</span>
+    <span className="badge badge-info">Info</span>
+    <span className="badge badge-light">Light</span>
+    <span className="badge badge-dark">Dark</span>
   </div>
 </main>
 

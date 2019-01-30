2b9e5e231dd4b6a066883c0b6ee90879176d884d
Adam Butterworth <abutterworth@edx.org>
2019-01-30 10:25:11 -0500

    Update to colors

--- src/pages/Basics.Colors.js
+++ src/pages/Basics.Colors.js
@@ -2,144 +2,185 @@ import React from 'react';
 import chroma from 'chroma-js';
 import MeasuredItem from '../common/MeasuredItem';
 
-const colors = {
-  primary: {},
-  secondary: {},
-  success: {},
-  info: {},
-  warning: {},
-  danger: {},
-  light: {},
-  dark: {},
-  'text-muted': {}
-};
 const bgColors = [
-  'white',
-  'primary',
-  'secondary',
-  'success',
-  'danger',
-  'warning',
-  'info',
-  'light',
-  'dark',
+  { name: 'White', value: '#fff' },
+  { name: 'BG Light Gray', value: '#fcfcfc' },
+  { name: 'BG Light Blue', value: '#f5f8ff' },
+  { name: 'BG Yellow', value: '#Fffdf3' },
 ];
 
 const textColors = [
-  'body',
-  'muted',
-  'white',
-  'black-50',
-  'white-50',
-  'primary',
-  'secondary',
-  'success',
-  'danger',
-  'warning',
-  'info',
-  'light',
-  'dark',
+  { name: 'Black 100', value: '#111' },
+  { name: 'Black 200', value: 'rgba(0,0,0,.75)' },
+  { name: 'Black 400', value: 'rgba(0,0,0,.55)' },
+  { name: 'Purple', value: '#45096b' },
+];
+const inverseTextColors = [
+  { name: 'White 100', value: '#fff' },
+  { name: 'White 200', value: 'rgba(255,255,255,.75)' },
+  { name: 'White 400', value: 'rgba(255,255,255,.55)' },
+  { name: 'Yellow', value: '#ffbf18' },
+];
+
+const semanticColors = [
+  { name: 'Interactive Blue', value: '#23419f' },
+  { name: 'Success', value: '#008100' },
+  { name: 'Danger', value: '#A2221D' },
+  { name: 'Warning', value: '#DDC047' },
+  { name: 'Info', value: '#4AA0B5' },
 ]
+const brandColors = [
+  { name: 'Magenta', value: '#b72667' },
+  { name: 'Grey', value: '#8a8c8f' },
+  { name: 'Blue', value: '#00a0e3' },
+  { name: 'Yellow', value: '#ffbf18' },
+];
+
+const programColors = [
+  { name: 'Masters Charcoal', value: '#414141' },
+  { name: 'Professional Certificate', value: '#9a1f60' },
+  { name: 'MicroMasters', value: '#065784' },
+];
+
+
+function Swatch({color, className, children, style}) {
+  return <div 
+    className={`${className}`}
+    style={{
+      backgroundColor: color,
+      ...style
+    }}
+  >{children}</div>
+}
+function TextSwatch({color, className, children, style}) {
+  return <span 
+    className={`${className}`}
+    style={{
+      color: color,
+      ...style
+    }}
+  >{children}</span>
+}
+
+function SwatchLabel({color, name, className, children, style}) {
+  return <label 
+    className={`${className}`}
+    style={{
+      ...style
+    }}
+  >{name} • {color}</label>
+}
 
 export default function Colors() {
   return (
-    <div>
+    <div className="pt-5">
+
+      <h5>Brand & Accents</h5>
+      <p>Use for accents only. These colors are not accessible as text or fill colors.</p>
+      <div className="row mb-5">{brandColors.map(({name, value}) => (
+        <div className="col">
+          <Swatch color={value} className="p-5 color-card" />
+          <SwatchLabel color={value} name={name} />
+        </div>
+      ))}</div>
+
       <div className="row mb-5">
-        {bgColors.map(color => (
-          <div className="col-2">
-            <MeasuredItem 
-              styles={{
-                'background-color': null
-              }} 
-              className="mb-3"
-              after
-              renderItem={(props, state) => {
-                return <div className={`p-4 bg-${color}`} />;
-              }}
-              renderValues={styleValues => {
-                if (!styleValues['background-color']) return;
+        <div className="col">
+          <h5>Text Colors (Primary)</h5>
+          <Swatch color={bgColors[0].value} className="p-5 color-card">
+            {textColors.map(({name, value}) => <TextSwatch color={value} className="d-block">{name} • {value}</TextSwatch> )}
+          </Swatch>
+        </div>
+        <div className="col">
+          <h5>Text Colors (Inverse)</h5>
+          <Swatch color={programColors[0].value} className="p-5 color-card">
+            {inverseTextColors.map(({name, value}) => <TextSwatch color={value} className="d-block">{name} • {value}</TextSwatch> )}
+          </Swatch>
+        </div>
+      </div>
 
-                const chromaColor = chroma(styleValues['background-color']);
-
-                return (
-                  <ul className="list-unstyled">
-                    <li>{color}</li>
-                    <li>{chromaColor.hex()}</li>
-                    <li>On white: {Math.round(10*chroma.contrast(chromaColor, 'white'))/10}</li>
-                  </ul>
-                );
-              }}
-            />
+      <h5>Semantic Colors</h5>
+      <p>Used both as text on light backgrounds and fill colors for white text (eg. buttons).</p>
+      <div className="row mb-5">
+      {semanticColors.map(({name, value}) => (
+        <div className="col-3">
+          <div className="color-card">
+            <Swatch color={value} className="p-3"><TextSwatch color={'#fff'}>{name}</TextSwatch></Swatch>
+            <Swatch color={'#fff'} className="pt-1 pb-3"><TextSwatch color={value}>{name} • {value}</TextSwatch></Swatch>
           </div>
-        ))}
+        </div>
+      ))}
       </div>
 
-
-      <h4>Colors Contrasts</h4>
-      <div className="row">
-      {
-        bgColors.map((color) => {
-          return (
-          <div className="col-4">
-            <MeasuredItem 
-              styles={{
-                'background-color': null
-              }} 
-              className="mb-3"
-              after
-              renderItem={(props, state) => {
-
-                return (
-                  <div className={`p-0 bg-${color}`} />
-                );
-              }}
-              renderValues={styleValues => {
-                if (!styleValues['background-color']) return;
-
-                const chromaColor = chroma(styleValues['background-color']);
-
-                return (
-                  <div className="mb-3">
-                    <h5>{color} • {chromaColor.hex()}</h5>
-
-                    <ul className={`bg-${color} p-4 list-unstyled m-0`}>
-                      {textColors.map((textColor) => {
-                        return (
-                          <li className="">
-                          <MeasuredItem 
-                              styles={{'color': null}} 
-                              after
-                              renderItem={(props, state) => {
-                                return <span className={`text-${textColor}`}></span>; 
-                              }}
-                              renderValues={styleValues => {
-                                if (!styleValues['color']) return;
-
-                                const chromaTextColor = chroma(styleValues['color']);
-                                const contrastRatio = Math.round(10*chroma.contrast(chromaTextColor, chromaColor))/10;
-                                if (contrastRatio < 3) return null;
-                                if (contrastRatio < 4.5) {
-                                  return <del className={`text-${textColor}`}>{textColor} - {contrastRatio}</del>;
-                                }
-
-                                return (
-                                  <span className={`text-${textColor}`}>{textColor} - {contrastRatio}</span>
-                                );
-                              }}
-                            />
-                            </li>
-                          );
-                      })}
-                    </ul>
-                  </div>
-                );
-              }}
-            />
-            </div>
-          );
-        })
-      }
+      <h5>Backgrounds</h5>
+      <p>Primary background colors.</p>
+      <div className="row mb-5">
+      {bgColors.map(({name, value}) => (
+        <div className="col-3 mb-3">
+          <Swatch color={value} className="p-5 color-card" />
+          <SwatchLabel color={value} name={name} />
+        </div>
+      ))}
       </div>
+      <h5>Program Backgrounds</h5>
+      <p>Used as backgrounds or accents for program related content.</p>
+      <div className="row mb-5">
+      {programColors.map(({name, value}) => (
+        <div className="col-3 mb-3">
+          <Swatch color={value} className="p-5 color-card" />
+          <SwatchLabel color={value} name={name} />
+        </div>
+      ))}
+      </div>
+      
+      {/*textColors.map(({name, value}) => <TextSwatch color={value} className="p-3">{name}</TextSwatch> )*/}
+
+      <h5>WIP Table view</h5>
+
+      
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Color</th>
+            <th>Text Black</th>
+            <th>Text Dark</th>
+            <th>Text Gray</th>
+            <th>Text White</th>
+            <th>Interactive Blue</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <h6>White</h6>
+              <p>#fff</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+
     </div>
   );
+}
+
+class Measure extends React.Component {
+  constructor(props) {
+    super(props);
+    this.nodeToMeasure = React.createRef();
+
+    this.state = {
+      styles: null
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      styles: getComputedStyle(this.nodeToMeasure.current)
+    })
+  }
+
+  render() {
+    return this.props.render(this.nodeToMeasure, this.state.styles);
+  }
 }

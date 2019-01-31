d7f7593b0d923da0363d66b98b3dee702d0edc70
Adam Butterworth <adamjbutterworth@gmail.com>
2019-01-30 21:40:49 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/edx-bootstrap/precache-manifest.04f14888163936f60e5ffa4884b406ac.js"
+  "/edx-bootstrap/precache-manifest.da86fc4487d822cc9ec9705f71449436.js"
 );
 
 workbox.clientsClaim();

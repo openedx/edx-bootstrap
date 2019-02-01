7f0b7c277e5d07537d76f752b2c8ed2cfb7496f2
David Joy <djoy@C02XQ24LJG5J.tld>
2019-02-01 17:39:32 -0500

    Fix some linting errors.

--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -85,28 +85,32 @@ export default class Forms extends React.Component {
           <form action="" className="needs-validation">
             <div className="bd-example">
               <div className="form-group">
-                <label htmlFor="exampleFormControlInput1">Cat's Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleFormControlInput1"
-                  placeholder="name@example.com"
-                  name="email"
-                  value={this.state.email}
-                  onChange={this.handleChange}
-                />
+                <label htmlFor="exampleFormControlInput1">
+                  Cat's Email address
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="name@example.com"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                  />
+                </label>
               </div>
               <div className="form-group">
-                <label htmlFor="inputPassword5">Secret Cat Password</label>
-                <input
-                  type="password"
-                  id="inputPassword5"
-                  className="form-control"
-                  aria-describedby="passwordHelpBlock"
-                  onChange={this.handleChange}
-                  value={this.state.password}
-                  name="password"
-                />
+                <label htmlFor="inputPassword5">
+                  Secret Cat Password
+                  <input
+                    type="password"
+                    id="inputPassword5"
+                    className="form-control"
+                    aria-describedby="passwordHelpBlock"
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                    name="password"
+                  />
+                </label>
                 <div className="valid-feedback">Looks good!</div>
                 <small id="passwordHelpBlock" className="form-text text-muted">
                   Your password must be 8-32 characters long, contain letters and numbers, and must
@@ -116,42 +120,42 @@ export default class Forms extends React.Component {
               <div className="form-group">
                 <label>Does your cat have mittens and boots?</label>
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="catClothes1"
-                    name="mittens"
-                    value={this.state.mittens}
-                    onChange={this.handleChange}
-                  />
                   <label className="form-check-label" htmlFor="catClothes1">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      id="catClothes1"
+                      name="mittens"
+                      value={this.state.mittens}
+                      onChange={this.handleChange}
+                    />
                     Mittens
                   </label>
                 </div>
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="catClothes2"
-                    name="boots"
-                    value={this.state.boots}
-                    onChange={this.handleChange}
-                  />
                   <label className="form-check-label" htmlFor="catClothes2">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      id="catClothes2"
+                      name="boots"
+                      value={this.state.boots}
+                      onChange={this.handleChange}
+                    />
                     Boots
                   </label>
                 </div>
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="checkbox"
-                    id="catClothes3"
-                    disabled
-                    name="notMittensOrBoots"
-                    value={this.state.notMittensOrBoots}
-                    onChange={this.handleChange}
-                  />
                   <label className="form-check-label" htmlFor="catClothes3">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      id="catClothes3"
+                      disabled
+                      name="notMittensOrBoots"
+                      value={this.state.notMittensOrBoots}
+                      onChange={this.handleChange}
+                    />
                     Neither
                   </label>
                 </div>
@@ -160,116 +164,124 @@ export default class Forms extends React.Component {
               <div className="form-group">
                 <label>How floofy is your cat?</label>
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="floofiness"
-                    id="floofiness1"
-                    value="very"
-                    checked={this.state.floofiness === 'very'}
-                    onChange={this.handleChange}
-                  />
                   <label className="form-check-label" htmlFor="floofiness1">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="floofiness"
+                      id="floofiness1"
+                      value="very"
+                      checked={this.state.floofiness === 'very'}
+                      onChange={this.handleChange}
+                    />
                     Very floofy
                   </label>
                 </div>
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="floofiness"
-                    id="floofiness2"
-                    value="somewhat"
-                    checked={this.state.floofiness === 'somewhat'}
-                    onChange={this.handleChange}
-                  />
                   <label className="form-check-label" htmlFor="floofiness2">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="floofiness"
+                      id="floofiness2"
+                      value="somewhat"
+                      checked={this.state.floofiness === 'somewhat'}
+                      onChange={this.handleChange}
+                    />
                     Somewhat floofy
                   </label>
                 </div>
                 <div className="form-check">
-                  <input
-                    className="form-check-input"
-                    type="radio"
-                    name="floofiness"
-                    id="floofiness3"
-                    disabled
-                    value="not"
-                    checked={this.state.floofiness === 'not'}
-                    onChange={this.handleChange}
-                  />
                   <label className="form-check-label" htmlFor="floofiness3">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="floofiness"
+                      id="floofiness3"
+                      disabled
+                      value="not"
+                      checked={this.state.floofiness === 'not'}
+                      onChange={this.handleChange}
+                    />
                     Not floofy
                   </label>
                 </div>
               </div>
 
               <div className="form-group">
-                <label htmlFor="exampleFormControlSelect1">How many cats do you have?</label>
-                <select
-                  className="form-control"
-                  id="exampleFormControlSelect1"
-                  name="catCount"
-                  value={this.state.catCount}
-                  onChange={this.handleChange}
-                >
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
+                <label htmlFor="exampleFormControlSelect1">
+                  How many cats do you have?
+                  <select
+                    className="form-control"
+                    id="exampleFormControlSelect1"
+                    name="catCount"
+                    value={this.state.catCount}
+                    onChange={this.handleChange}
+                  >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </label>
               </div>
               <div className="form-group">
-                <label htmlFor="exampleFormControlSelect2">Which is your cat's name?</label>
-                <select
-                  size={5}
-                  className="form-control"
-                  id="exampleFormControlSelect2"
-                  name="catName"
-                  value={this.state.catName}
-                  onChange={this.handleChange}
-                >
-                  <option>Snowball</option>
-                  <option>Taco</option>
-                  <option>Dr. Teeth</option>
-                  <option>Albus Dumblemeow</option>
-                  <option>Kitty</option>
-                </select>
+                <label htmlFor="exampleFormControlSelect2">
+                  Which is your cat's name?
+                  <select
+                    size={5}
+                    className="form-control"
+                    id="exampleFormControlSelect2"
+                    name="catName"
+                    value={this.state.catName}
+                    onChange={this.handleChange}
+                  >
+                    <option>Snowball</option>
+                    <option>Taco</option>
+                    <option>Dr. Teeth</option>
+                    <option>Albus Dumblemeow</option>
+                    <option>Kitty</option>
+                  </select>
+                </label>
               </div>
               <div className="form-group">
-                <label htmlFor="exampleFormControlDate">When was your cat born?</label>
-                <input
-                  type="date"
-                  className="form-control"
-                  name="birthday"
-                  value={this.state.birthday}
-                  onChange={this.handleChange}
-                />
+                <label htmlFor="exampleFormControlDate">
+                  When was your cat born?
+                  <input
+                    type="date"
+                    className="form-control"
+                    name="birthday"
+                    value={this.state.birthday}
+                    onChange={this.handleChange}
+                  />
+                </label>
               </div>
               <div className="form-group">
                 <label htmlFor="exampleFormControlTextarea1">
                   Describe your cat's favorite napping spot
+                  <textarea
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    name="nappingSpot"
+                    value={this.state.nappingSpot}
+                    rows={3}
+                    onChange={this.handleChange}
+                  />
                 </label>
-                <textarea
-                  className="form-control"
-                  id="exampleFormControlTextarea1"
-                  name="nappingSpot"
-                  value={this.state.nappingSpot}
-                  rows={3}
-                  onChange={this.handleChange}
-                />
               </div>
               <div className="form-group">
-                <label htmlFor="exampleFormControlFile1">Upload a picture of your cat</label>
-                <input
-                  type="file"
-                  className="form-control-file"
-                  name="catPicture"
-                  value={this.state.catPicture}
-                  id="exampleFormControlFile1"
-                  onChange={this.handleChange}
-                />
+                <label htmlFor="exampleFormControlFile1">
+                  Upload a picture of your cat
+                  <input
+                    type="file"
+                    className="form-control-file"
+                    name="catPicture"
+                    value={this.state.catPicture}
+                    id="exampleFormControlFile1"
+                    onChange={this.handleChange}
+                  />
+                </label>
               </div>
               <button type="submit" className="btn btn-primary mr-2" onClick={this.handleSubmit}>
                 Submit

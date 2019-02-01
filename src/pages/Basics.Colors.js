8b164046dc5422efff3b26a5d8a9b6ffbcdce1ef
Adam Butterworth <abutterworth@edx.org>
2019-02-01 13:08:43 -0500

    Major update to colors

--- src/pages/Basics.Colors.js
+++ src/pages/Basics.Colors.js
@@ -3,42 +3,185 @@ import chroma from 'chroma-js';
 import MeasuredItem from '../common/MeasuredItem';
 import DocSection from '../DocSection';
 
+const colors = {
+  'gray-900': {
+    value: '#111',
+    name: 'Gray 900',
+    utilityClassName: 'gray-900',
+    forBG: false,
+    forText: true,
+  },
+  'gray-700': {
+    value: '#414141',
+    name: 'Gray 700',
+    utilityClassName: 'gray-700',
+    forBG: false,
+    forText: true,
+  },
+  'gray-600': {
+    value: 'rgba(65,65,65,.7)',
+    name: 'Gray 600',
+    utilityClassName: 'gray-600',
+    forBG: false,
+    forText: true,
+  },
+  'primary': {
+    value: '#23419F',
+    name: 'UI Blue (Primary)',
+    utilityClassName: 'primary',
+    forBG: true,
+    forText: true,
+  },
+  'secondary': {
+    value: '#45096B',
+    name: 'UI Indigo (Secondary)',
+    utilityClassName: 'secondary',
+    forBG: true,
+    forText: true,
+  },
+  'success': {
+    value: '#008100',
+    name: 'UI Success',
+    utilityClassName: 'success',
+    forBG: true,
+    forText: true,
+  },
+  'info': {
+    value: '#065784',
+    name: 'Text Info',
+    utilityClassName: 'info',
+    forBG: false,
+    forText: true,
+  },
+  'danger': {
+    value: '#A2221D',
+    name: 'UI Danger',
+    utilityClassName: 'danger',
+    forBG: true,
+    forText: true,
+  },
+  'fill-success': {
+    value: '#DFF0D8',
+    name: 'Success Fill',
+    utilityClassName: 'fill-success',
+    forBG: true,
+    forText: false,
+  },
+  'fill-info': {
+    value: '#D8EDF8',
+    name: 'Info Fill',
+    utilityClassName: 'fill-info',
+    forBG: true,
+    forText: false,
+  },
+  'fill-danger': {
+    value: '#F2DEDE',
+    name: 'Danger Fill',
+    utilityClassName: 'fill-danger',
+    forBG: true,
+    forText: false,
+  },
+  'fill-warning': {
+    value: '#FCF8E2',
+    name: 'Warning Fill',
+    utilityClassName: 'fill-warning',
+    forBG: true,
+    forText: false,
+  },
+  'program-masters': {
+    value: '#414141',
+    name: 'Masters',
+    utilityClassName: 'program-masters',
+    forBG: true,
+    forText: false,
+  },
+  'program-micro-masters': {
+    value: '#065784',
+    name: 'MicroMasters',
+    utilityClassName: 'program-micro-masters',
+    forBG: true,
+    forText: false,
+  },
+  'program-prof-cert': {
+    value: '#9A1F60',
+    name: 'Professional Certificate',
+    utilityClassName: 'program-prof-cert',
+    forBG: true,
+    forText: false,
+  },
+  'white': {
+    value: '#fff',
+    name: 'White',
+    utilityClassName: 'white',
+    forBG: true,
+    forText: true,
+  },
+  'pale-gray': {
+    value: '#FCFCFC',
+    name: 'Pale Gray',
+    utilityClassName: 'pale-gray',
+    forBG: true,
+    forText: false,
+  },
+  'pale-blue': {
+    value: '#F5F8FF',
+    name: 'Pale. Blue',
+    utilityClassName: 'pale-blue',
+    forBG: true,
+    forText: false,
+  },
+  'pale-yellow': {
+    value: '#FFFDF3',
+    name: 'Pale Yellow',
+    utilityClassName: 'pale-yellow',
+    forBG: true,
+    forText: false,
+  },
+  'yellow': {
+    value: '#FFBF18',
+    name: 'Accent Yellow',
+    utilityClassName: 'yellow',
+    forBG: true,
+    forText: false,
+  },
+};
 
 const textAndInteractiveColors = [
-  { name: 'Black 100', value: '#111', forText: true, forFill: false, labelColor: '#fff' },
-  { name: 'Black 200', value: '#414141', forText: true, forFill: false, labelColor: '#fff' },
-  { name: 'Black 400', value: 'rgba(65,65,65,0.70)', forText: true, forFill: false, labelColor: '#fff' },
-  { name: 'Interactive UI', value: '#23419F', forText: true, forFill: true, labelColor: '#fff' },
-  { name: 'Purple UI', value: '#45096B', forText: true, forFill: true, labelColor: '#fff' },
+  'gray-900',
+  'gray-700',
+  'gray-600',
+  'primary',
+  'secondary',
 ];
 
 const programColors = [
-  { name: 'Masters', value: '#414141', forText: false, forFill: true, labelColor: '#fff' },
-  { name: 'MicroMasters', value: '#065784', forText: false, forFill: true, labelColor: '#fff' },
-  { name: 'Prof Cert', value: '#9A1F60', forText: false, forFill: true, labelColor: '#fff' },
+  'program-masters',
+  'program-micro-masters',
+  'program-prof-cert',
 ];
 
 const accentColors = [
-  { name: 'Yellow', value: '#FFBF18', forText: false, forFill:true, labelColor: '#000' },
+  'yellow',
 ];
 
 const systemColorsBG = [
-  { name: 'Success BG', value: '#DFF0D8', forText: false, forFill: true, labelColor: '#000' },
-  { name: 'Info BG', value: '#D8EDF8', forText: false, forFill: true, labelColor: '#000' },
-  { name: 'Danger BG', value: '#F2DEDE', forText: false, forFill: true, labelColor: '#000' },
-  { name: 'Warning BG', value: '#FCF8E2', forText: false, forFill: true, labelColor: '#000' },
+  'fill-success',
+  'fill-info',
+  'fill-danger',
+  'fill-warning',
 ];
+
 const systemColorsUI = [
-  { name: 'Success UI', value: '#008100', forText: true, forFill: true, labelColor: '#fff' },
-  { name: 'Info Text', value: '#065784', forText: true, forFill: false, labelColor: '#fff' },
-  { name: 'Danger UI', value: '#A2221D', forText: true, forFill: true, labelColor: '#fff' },
-]
+  'success',
+  'info',
+  'danger',
+];
 
 const bgColors = [
-  { name: 'White', value: '#FFF', forText: false, forFill: true, labelColor: '#000' },
-  { name: 'Light Gray BG', value: '#FCFCFC', forText: false, forFill: true, labelColor: '#000' },
-  { name: 'Light Blue BG', value: '#F5F8FF', forText: false, forFill: true, labelColor: '#000' },
-  { name: 'Yellow BG', value: '#FFFDF3', forText: false, forFill: true, labelColor: '#000' },
+  'white',
+  'pale-gray',
+  'pale-blue',
+  'pale-yellow',
 ];
 
 
@@ -78,13 +221,15 @@ function TextSwatch({color, bgColor, className, children, style}) {
 }
 
 export default function Colors() {
-  const renderColor = ({name, value, forFill, forText, labelColor}) => {
+  const renderColor = (colorKey) => {
+    const {name, value, forBG, forText } = colors[colorKey];
+    const contrastColor = chroma(value).luminance() > .5 ? '#111' : '#fff';
     return (
       <div className="col-md-6 col-lg-5 col-xl-4 mb-3">
         <label className="color-label">{name}</label>
         <Tile>
-          {forFill ? <Swatch color={value} textColor={labelColor} /> : null}
-          {forText ? <TextSwatch color={value}>{value} <span className="text-swatch-sample">Aa</span></TextSwatch> : null}
+          {forBG  ? <Swatch color={value} textColor={contrastColor} /> : null}
+          {forText ? <TextSwatch color={value} style={{backgroundColor: contrastColor}}>{value} <span className="text-swatch-sample">Aa</span></TextSwatch> : null}
         </Tile>
       </div>
     );
@@ -107,6 +252,64 @@ export default function Colors() {
       
       <h5 className="section">Backgrounds</h5>
       <div className="row mb-3">{bgColors.map(renderColor)}</div>
+
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name/Value</th>
+            <th colSpan={3}>Utility Classes</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(colors).map(colorKey => {
+            const {
+              name,
+              value,
+              utilityClassName,
+              backgroundUtilityClass,
+              forText,
+              forBG,
+            } = colors[colorKey];
+
+            const contrastColor = chroma(value).luminance() > .5 ? '#000' : '#fff';
+
+            return (
+              <tr>
+                <td>
+                  {name}<br />
+                  {value}
+                </td>
+                <td className={utilityClassName}>
+                  {forText ? (
+                    <code 
+                      className={`text-${utilityClassName}`}
+                      style={{backgroundColor: contrastColor}}
+                    >
+                      .text-{utilityClassName}
+                    </code>
+                  ) : null}
+                </td>
+                <td>
+                  {forBG ? (
+                    <code
+                      className={`d-block bg-${utilityClassName} p-1`}
+                      style={{color: contrastColor}}
+                    >
+                      .bg-{utilityClassName}
+                    </code>
+                  ) : null}
+                </td>
+                <td>
+                  <code className={`border border-${utilityClassName} p-1`}>
+                    .border-{utilityClassName}
+                  </code>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }

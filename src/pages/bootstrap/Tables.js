ef2425417c4bc0d8b3e012d4d4cbe50371c08ba0
Adam Butterworth <abutterworth@edx.org>
2019-06-11 13:41:58 -0600

    Tables, Button focus, Input borders (#56)
    * fix: use mixin for mobile type so it can be used elsewhere
    
    * feat: button focus states now match color of button
    
    * fix: increase border width of inputs. remove default table style
    
    * docs: update tables
    
    * fix: make inputs 1px again

--- src/pages/bootstrap/Tables.js
+++ src/pages/bootstrap/Tables.js
@@ -11,15 +11,44 @@ export default class Tables extends React.Component {
     <li>Todo responsive, tables become lists.</li>
   </ul>
 
-  <h2>Notes</h2>
-  <ul>
-    <li>Our base table is bordered.</li>
-  </ul>
+  <h2 id="bordered-table"><span className="bd-content-title">Borderless table</span></h2>
   
+  <div className="bd-example">
+    <table className="table table-borderless">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
   <h2 id="bordered-table"><span className="bd-content-title">Bordered table</span></h2>
   
   <div className="bd-example">
-    <table className="table">
+    <table className="table table-bordered">
       <thead>
         <tr>
           <th scope="col">#</th>

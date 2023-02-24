43d3c1d179860aaaf30b6e38a8e1483269aef413
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2023-02-24 11:25:02 +0500

    Bumped stylelint-config-edx & node-sass, resolved stylelint issues (#210)
    * chore: bumped stylelint-config-edx & node-sass
    
    * refactor: resolve stylelint issues
    
    * refactor: added linespace in stylelint.config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   extends: '@edx/stylelint-config-edx',
   "rules": {
-    "at-rule-empty-line-before": null
+    "at-rule-empty-line-before": null,
+    "function-no-unknown": null,
+    "scss/no-global-function-names": null,
   }
 };
